ea7b986fa97cb1ff8c86865fd5bf76580e3b2071
camarcano <c.marcano@gmail.com>
2023-02-19 00:56:16 -0600

    Added: Dataframe created with all info for each pitch

--- vid-savant-ver2.00.py
+++ vid-savant-ver2.00.py
@@ -1,25 +1,31 @@
-### Getting videos from Baseball Savant ###
-import requests
-import urllib.request
+# ## Getting videos from Baseball Savant ###
+
+# import urllib.request
 import re
 import os
-import wget
-import youtube_dl
 import glob
 import shutil
+import requests
+import youtube_dl
+from bs4 import BeautifulSoup
+import pandas as pd
+import lxml
 
-def find_video_links(webpageHTML):
+
+def find_video_links(webpage_html):
     expression = r'"(/sporty-v.*?)" target'
-    return re.findall(expression, webpageHTML)
+    return re.findall(expression, webpage_html)
+
+def find_pitch_types(webpage_html):
+    expression = r'search-pitch-label-.*?</span>'
+    return re.findall(expression, webpage_html)
+
+    
 
 def download_video(url, name):
-    url2=f"https://baseballsavant.mlb.com{url}"
-    #command = f"youtube-dl https://baseballsavant.mlb.com{url} -o {name}.mp4"
-    #os.system(command)
-    #urllib.request.urlretrieve(url2,dest)
-    #filename = wget.download(url)
+    url_2 = f"https://baseballsavant.mlb.com{url}"
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url2])
+        ydl.download([url_2])
 
 # Downloads the list of given URLs with youtbe-dl
 def download_all_matches(matches):
@@ -32,14 +38,12 @@ def download_all_matches(matches):
 def rename(directory):
     os.chdir(directory)
     num = 1
-    for file in [file for file in sorted(os.listdir(), key=os.path.getctime, reverse=True) if os.path.splitext(file)[1] == ".mp4"]:
+    for file in [file for file in sorted(os.listdir(), key=os.path.getctime, reverse=False) if os.path.splitext(file)[1] == ".mp4"]:
         if os.path.splitext(file)[1] == ".mp4":
             os.rename(file, "name{:03d}.mp4".format(num))
             num += 1
 
-
 ydl_opts = {}
-
 
 # Expects date arguments in the format 2019-05-11
 start_date = input("Enter start date (YYYY-MM-DD): ")
@@ -51,39 +55,47 @@ is_last_pitch_str = input("Is last pitch (True/False)? ")
 is_last_pitch = is_last_pitch_str.lower() == "true"
 flag = "is...last...pitch|" if is_last_pitch else ""
 url = f"https://baseballsavant.mlb.com/statcast_search?hfPTM=&hfPT=&hfAB=&hfGT=R%7C&hfPR=&hfZ=&hfStadium=&hfBBL=&hfNewZones=&hfPull=&hfC=&hfSea={season}%7C&hfSit=&player_type=pitcher&hfOuts=&hfOpponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt={start_date}&game_date_lt={end_date}&hfMo=&hfTeam=&home_road=&hfRO=&position=&hfInfield=&hfOutfield=&hfInn=&hfBBT=&hfFlag={flag}&pitchers_lookup%5B%5D={player_id}&metric_1=&group_by=name&min_pitches=0&min_results=0&min_pas=0&sort_col=pitches&player_event_sort=api_p_release_speed&sort_order=desc&type=details&player_id={player_id}"
-#url = f"https://baseballsavant.mlb.com/statcast_search?hfPTM=&hfPT=&hfAB=&hfGT=R%7C&hfPR=&hfZ=&hfStadium=&hfBBL=&hfNewZones=&hfPull=&hfC=&hfSea=2022%7C&hfSit=&player_type=pitcher&hfOuts=&hfOpponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=2022-05-01&game_date_lt=2022-05-15&hfMo=&hfTeam=&home_road=&hfRO=&position=&hfInfield=&hfOutfield=&hfInn=&hfBBT=&hfFlag=&pitchers_lookup%5B%5D=605400&metric_1=&group_by=name&min_pitches=0&min_results=0&min_pas=0&sort_col=pitches&player_event_sort=api_p_release_speed&sort_order=desc&type=details&player_id=605400"
 r = requests.get(url, allow_redirects=True)
-#open('site.txt', 'wb').write(r.content)
-#with urllib.request.urlopen(url) as response:
-#    html = response.read().decode()
 html = r.content
+
 open('site.txt', 'wb').write(html)
 
-with open('site.txt') as f:
+with open('site.txt', encoding='utf-8') as f:
     lines = f.read()
 
 matches = find_video_links(lines)
-# open file in write mode
-with open(r'links.txt', 'w') as fp:
-    for item in matches:
-        # write each item on a new line
-        fp.write("%s\n" % item)
+matches.reverse()
+
+soup = BeautifulSoup(r.text, "html.parser")
+# Obtain information from tag <table>
+table1 = soup.find('table', id=f"ajaxTable_{player_id}")
+
+# Obtain every title of columns with tag <th>
+headers = []
+for i in table1.find_all('th'):
+    title = i.text
+    headers.append(title)
+
+
+df = pd.read_html(lines, encoding='latin1', header=0)[0]
+del df[df.columns[-1]]
+df = df.loc[::-1].reset_index(drop=True)
+
 
 if len(matches) <= 1:
     print("ERROR, 0 matches found in request")
-    #cleanup_leftover_files()
     exit()
 
 download_all_matches(matches)
 rename(os.getcwd())
 
 src_folder = os.getcwd()
-dst_folder = os.getcwd() + "/videos/"
+dst_folder = os.getcwd() + "/vids/"
 
 try: 
     os.mkdir(dst_folder) 
 except OSError as error: 
-    print("Directory 'videos' already exists") 
+    print(error) 
 
 
 # Search files with .mp4 extension in source directory
@@ -97,4 +109,3 @@ for file in files:
     shutil.move(file, dst_folder + file_name)
     print('Moved:', file)
 
-### TESTING GIT
